22ecd1571fba2bc3fb86491fd9756c8bd1c8e5ad
unknown <01fe22bca075@kletech.ac.in>
2024-02-18 23:37:12 +0530

    program for student deatils

---
+++ Student_Course.java
@@ -0,0 +1,26 @@
+public class Student_Course extends Student {
+    void course()
+    {
+        String[] subjects = {"DevOps", "Machine Learning", "C#"};
+        int[][] marks = {
+            {50, 30, 60} 
+             
+        };
+
+        // Assuming each row represents a student and each column represents a subject
+        for (int i = 0; i < marks.length; i++) {
+            for (int j = 0; j < marks[i].length; j++) {
+                if (marks[i][j] < 40) {
+                    System.out.println("samarth " + (i + 1) + " scored less than 40 in the " + subjects[j]);
+                }
+            }     
+        }
+        }
+    public static void main(String args[])
+    {
+        Student_Course sc = new Student_Course();
+        sc.stud_det();
+        sc.course();
+        
+    }
+}
